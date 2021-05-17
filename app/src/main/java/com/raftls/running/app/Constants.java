2ceb809ed71f2f5f5caf59836848d63f2806619d
rvasseur31 <raphael.vasseur@ynov.com>
2021-05-17 21:10:24 +0200

    Add production server url

--- app/src/main/java/com/raftls/running/app/Constants.java
+++ app/src/main/java/com/raftls/running/app/Constants.java
@@ -1,6 +1,5 @@
 package com.raftls.running.app;
 
 public class Constants {
-    public static final String BASE_URL = "http://192.168.1.13:3000";
-    //public static final String BASE_URL = "http://192.168.3.12:3000";
+    public static final String BASE_URL = "https://ynov-running.herokuapp.com/";
 }
